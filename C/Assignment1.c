d66cfce00d9d6a0a66b74310c50e0314cb0c3c08
Shreyas-halmare-2005 <halmareshreyas43@gmail.com>
2026-09-25 13:53:57 +0530

    Add files via upload

---
+++ C/Assignment1.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+
+//1.check whether a number is even or odd
+
+//void main(){
+//	int no= 125;
+//	if( no%2==0){
+//		printf("no is even");
+//	}
+//	else{
+//		printf("no is odd");
+//	}
+//}
+
+
+//2.check whether a given year is a leap year
+
+//void main(){
+//      int year= 1990;	
+//	if(year%4==0 && year%100!=0 || year%400==0){
+//		printf("this year is leaf");
+//	}else{
+//		printf("this is not a leaf year");
+//	}
+//}
+
+
+//3.check whether number is positive or negative
+
+//void main(){
+//	int no=-29;
+//	if(no>0){
+//		printf("is positive");
+//	}else{
+//		printf("is negative");
+//	}
+//}
+
+
+//4.check whether uppercase or lowercase
+
+//void main(){
+//	char ch= 'E';
+//	if (ch>='A'&& ch<='Z'){
+//		printf("this is upper case");
+//	}else{
+//		printf("this is lower case");
+//	}
+//}
+
+
+//5. check given 3 digit number is pallindrome or not.
+
+//void main(){
+//	int a=12321,r=0;
+//	if(a==r){
+//		printf("It is Palindrom");
+//	} else{
+//	    printf("It is not Palindrom");	
+//	}
+//	}
+
+
+//6.check whether a person is eligible to vote (age ≥ 18). 
+
+//void main(){
+//	int a=25;
+//	if(a>=18){
+//		printf("It can vote");
+//	}else {
+//		printf("It cannot vote");
+//	}
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
